012483069a587a11b430c9e504cfaf60de921dc5
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-16 14:42:33 -0300

    Módulo 23

---
+++ 03 Fundamentos/023 Inferência de Tipos/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Inferência de tipos
+        // Pode-se usar inferência na declaração do tipo de uma variável,
+        // veja:
+
+        var a = 4.5; // Usando "var", o compilador irá automaticamente designar um tipo para a variável, que nesse caso
+                     // é double. É necessário inicializá-la juntamente ao "var" também.
+        System.out.println("Variável a: "+a);
+
+    }
+
+}
