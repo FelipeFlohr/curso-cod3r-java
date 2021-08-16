4f979c0d3bbf6790e749e1ae74904ea8fa32e430
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-16 13:00:10 -0300

    Tutorial 15

--- 03 Fundamentos/014 Primeiro programa/src/Main.java
+++ 03 Fundamentos/014 Primeiro programa/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // Print no Java
         System.out.println("Salve o Corinthians");
+        System.out.println("Uma vez Flamengo");
+        System.out.println("No remo és imortal, no futebol és um traço, de união Brasil-Portugal!");
     }
 
 }
