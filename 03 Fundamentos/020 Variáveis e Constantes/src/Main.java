4ccab298531a324016a90410ddf1ab811a26e2b4
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-16 14:02:59 -0300

    Módulo 20

---
+++ 03 Fundamentos/020 Variáveis e Constantes/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Declarando uma variável ou constante
+        // Variável -> Tipo + nome da variável = valor
+        // Constante -> final + tipo + NOME DA VARIÁVEL = valor
+        double raio = 3.4;
+        final double PI = 3.14159;
+        double area = PI * raio * raio;
+
+        System.out.println(area);
+    }
+
+}
