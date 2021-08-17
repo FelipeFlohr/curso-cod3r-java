7153251b5a601a83e2f3a63202d63d6b4f6ea55b
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-17 13:55:13 -0300

    Módulo 31

---
+++ 03 Fundamentos/031 Objeto vs Primitivo/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Primitivos vs Objetos
+
+        String s = "texto"; // A variável "s" é um objeto, pois possui atributos e comportamentos
+        s.toUpperCase(); // Este é um comportamento da variável "s"
+
+        // Em contrapartida, tipos primitivos apenas carregam valores, contudo, para atribuir coisas e comportamentos á
+        // um primitivo, existe as classes Wrapper
+        int a = 123; // A variável "a" é um tipo primitivo
+    }
+
+}
