645381026926ce23794cbf2d9fc6acfbafb61d98
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-17 14:11:01 -0300

    Módulo 32

---
+++ 03 Fundamentos/032 Wrappers/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Wrapper
+        // Wrapper vem do inglês "embrulho", "embrulhador"...
+
+        Byte b = 100; // Classe wrapper do "byte"
+        Short s = 1000; // Classe wrapper do "short"
+        Integer i = 10000; // Classe wrapper do "int"
+        Long l = 100000L; // Classe wrapper do "lang"
+        Double d = 1.13; // Classe wrapper do "double"
+        Float f = 1.13f; // Classe wrapper do "float"
+
+        Boolean bool = true; // Classe wrapper do "boolean"
+        Character c = '@'; // Classe wrapper do "character"
+        String string = "Felipe"; // String por si só é um objeto, ou seja, não há Wrapper
+    }
+
+}
