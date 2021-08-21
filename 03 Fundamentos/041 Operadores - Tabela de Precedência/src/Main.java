952feef6e2c311c44c0a274eade190516cc0d25e
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-21 12:14:21 -0300

    Módulo 41

---
+++ 03 Fundamentos/041 Operadores - Tabela de Precedência/src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+
+    /*Operadores Java - Tabela de precedência
+    Precedência | Operador   | Tipo                                | Associatividade
+    ------------------------------------------------------------------------------------
+    1           | =          | Atribuição                          | Direita p/ Esquerda
+                | +=         | Atribuição aditiva                  |
+                | -=         | Atribuição subtrativa               |
+                | *=         | Atribuição de multiplicação         |
+                | /=         | Atribuição de divisão               |
+                | %=         | Atribuição de módulo                |
+    ------------------------------------------------------------------------------------
+    2           | ?:         | Condição ternária                   | Direita p/ Esquerda
+    ------------------------------------------------------------------------------------
+    3           | ||         | OU lógico                           | Esquerda p/ Direita
+    ------------------------------------------------------------------------------------
+    4           | &&         | E lógico                            | Esquerda p/ Direita
+    ------------------------------------------------------------------------------------
+    5           | |          | Ou lógico (bit a bit)               | Esquerda p/ Direita
+    ------------------------------------------------------------------------------------
+    6           | ^          | Ou exclusivo (bit a bit)            | Esquerda p/ Direita
+    ------------------------------------------------------------------------------------
+    7           | &          | E lógico (bit a bit)                | Esquerda p/ Direita
+    ------------------------------------------------------------------------------------
+    8           | ==         | Igualdade                           | Esquerda p/ Direita
+                | !=         | Desigualdade                        |
+    ------------------------------------------------------------------------------------
+    9           | <          | Menor que                           | Esquerda p/ Direita
+                | <=         | Menor que ou igual                  |
+                | >          | Maior que                           |
+                | >=         | Maior que ou igual                  |
+                | instanceof | Comparação de tipos (objects only)  |
+    ------------------------------------------------------------------------------------
+    10          | <<         | Deslocamento aritmético p/ esquerda | Esquerda p/ Direita
+                | >>         | Deslocamento aritmética p/ direita  |
+                | >>>        | Deslocamento lógico p/ direita      |
+    ------------------------------------------------------------------------------------
+    11          | +          | Adição                              | Esquerda p/ Direita
+                | -          | Subtração                           |
+    ------------------------------------------------------------------------------------
+    12          | *          | Multiplicação                       | Esquerda p/ Direita
+                | /          | Divisão                             |
+                | %          | Módulo                              |
+    ------------------------------------------------------------------------------------
+    13          | ++         | Incremento pré-fixado               | Direita p/ Esquerda
+                | --         | Decremento pré-fixado               |
+                | +          | Mais                                |
+                | -          | Menos                               |
+                | !          | Negação lógica                      |
+                | ~          | Complemento binário                 |
+                | (type)     | Conversão explícita (cast)          |
+    ------------------------------------------------------------------------------------
+    14          | ++         | Incremento pós-fixado               | Direita p/ Esquerda
+                | --         | Decremento pós-fixado               |
+    ------------------------------------------------------------------------------------
+    15          | ()         | Parentêses                          | Esquerda p/ Direita
+                | []         | Seleção em array                    |
+                | ∙          | Seleção de membros                  |
+    ------------------------------------------------------------------------------------
+     */
+
+}
