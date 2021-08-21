2826aef748cf16cb843e450a0dbd0af933f93d8c
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-21 11:51:47 -0300

    Módulo 40

---
+++ 03 Fundamentos/040 Operadores/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // No Java há 3 principais tipos de distinção de operadores: Unários, binários, ternário
+        /* Exemplo de operadores
+        Unário: a++ <- Unário pois há apenas um operando
+        Binário: 3 + 2 <- Binário pois há apenas dois operandos
+        Ternário: v ? v : v <- Ternário pois há três operandos e dois operadores
+         */
+
+        // Prefix, postfix & infix
+        // Prefix: ++a (operador e operando)
+        // Postfix: a++ (operando e operador)
+        // Infix: 3 + 2 (operador, operando e operador)
+    }
+
+}
