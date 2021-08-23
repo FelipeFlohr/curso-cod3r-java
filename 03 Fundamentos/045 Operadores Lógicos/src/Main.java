b97ffe386db2809ffbcc01fc4e1fbe5080af9ab7
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-23 13:11:32 -0300

    Módulo 45

---
+++ 03 Fundamentos/045 Operadores Lógicos/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Operadores lógicos
+        /* Tabela verdade:
+        ------------------------------
+        Condição 1 E Condição 2 (&&) (Inglês: AND)
+        V          E          V -> V
+        V          E          F -> F
+        F          E          V -> F
+        F          E          F -> F
+        ------------------------------
+        Condição 1 OU Condição 2 (||) (Inglês: OR)
+        V          OU          V -> V
+        V          OU          F -> V
+        F          OU          V -> V
+        F          OU          F -> F
+        ------------------------------
+        Condição 1 XOR Condição 2 (^) (Inglês: XOR)
+        V          XOR          V -> F
+        V          XOR          F -> V
+        F          XOR          V -> V
+        F          XOR          F -> F
+         */
+
+    }
+
+}
