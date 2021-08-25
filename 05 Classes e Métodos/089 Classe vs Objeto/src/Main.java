6efd4e74e0c172e7d0c5ae2a3e53c292daad62c8
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-25 12:27:27 -0300

    Módulo 89

---
+++ 05 Classes e Métodos/089 Classe vs Objeto/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Classe vs Objeto
+        // Classe define o tipo e objeto define os dados
+
+        // Anatomia de uma classe
+        /* class Nome <- nome da classe em letra maiúscula
+
+        Corpo de uma classe é composto por atributos e comportamentos
+        - Atributos pode ser interpretado como características/dados da classe
+        - Comportamentos pode ser definido como métodos da classe
+        */
+
+        // Fatos sobre classes
+        // - Uma classe define um tipo de estrutura de dados
+        // - Uma classe representa uma abstração.
+
+        // Também pode-se chamar o objeto de instância
+
+        // Exemplo de utilização de classe:
+        /* Classe Produto:
+         - Nome
+         - Preço
+         - Desconto
+
+         Instância 1:
+         - Notebook
+         - 4200
+         - 15%
+
+         Instância 2:
+         - Celular
+         - 1800
+         - 35% */
+
+    }
+
+}
