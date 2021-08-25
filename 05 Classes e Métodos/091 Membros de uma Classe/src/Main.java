59aaf33a93fa975f94240c1ea3b8a390b2aaada5
Felipe Flohr <felipeflohrlol@gmail.com>
2021-08-25 12:40:34 -0300

    Módulo 91

---
+++ 05 Classes e Métodos/091 Membros de uma Classe/src/Main.java
@@ -0,0 +1,24 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        // Membros de uma classe:
+        // Variáveis / Constantes
+        // Construtores
+        // Métodos
+        // Classes (é possível ter uma classe dentro de outra)
+
+        /* Os membros da classe podem ser divididos em atributos e comportamentos
+        Atributos:      Variáveis  -> Classe <- Pode ser static
+                                   -> Objeto
+
+                        Constantes -> Classe <- Pode ser static
+                                   -> Objeto
+        Comportamentos: Métodos    -> Classe <- Pode ser static
+                                   -> Objeto
+                        Construtores
+        */
+
+    }
+
+}
