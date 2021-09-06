a9350c15eb1b262576a611ee074469eaf0fed2d8
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-06 00:27:57 -0300

    Módulo 111

---
+++ 05 Classes e Métodos/111 This e This()/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Palavra-chave "this"
+
+        // A palavra-chave "this" serve para se referir ao ob-
+        // jeto atual.
+
+        Data d1 = new Data(6, 9, 2021);
+        d1.imprimirDataFormatada();
+
+    }
+
+}
