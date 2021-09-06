33a904171bb33c55d773c7e0d095229827fe3d41
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-06 13:38:05 -0300

    Módulo 128

---
+++ 06 Arrays e Collections/128 Collections/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Collections
+        // Para setar os tipos nas collections, usa-se classes wrapper
+
+        /* Tipos de collections:
+
+        Set:
+        - Não ordenado (por padrão)
+        - Não indexado
+        - Não aceita repetição
+
+        List:
+        - Indexada
+        - Aceita repetição
+
+        Map:
+        - Chave/Valor
+        - Chave não aceita repetição
+        - Valor aceita repetição
+
+        Queue:
+        - Implementa fila
+        - First in/First out (FiFo, primeiro dado a entrar é o primeiro a sair)
+
+        Stack:
+        - Implementa pilhja (stack)
+        - Last in / First out (LiFo, último dado a entrar é o primeiro a sair)
+        */
+    }
+}
