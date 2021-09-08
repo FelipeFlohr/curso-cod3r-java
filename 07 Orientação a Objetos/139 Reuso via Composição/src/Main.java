37fffd2b031ceb210f23fe9601171e0af2235650
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-08 19:56:42 -0300

    Módulo 139

---
+++ 07 Orientação a Objetos/139 Reuso via Composição/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Reuso via Composição
+        // Nome dado á mecânica de separar várias funcionalida-
+        // des em várias classes
+
+        /* Exemplos:
+        Dois objetos: carro e motor
+        Carro <---> Motor (Relação de um pra um)
+
+        Mais de dois objetos relacionados para um: Compra e
+        Item (item pode ser vários)
+        Compra <---> Item (Relação de um para muitos)
+
+        Objetos relacionando entre si: Tio e Sobrinho (um tio
+        pode ter mais de um sobrinho e vice-versa)
+        Tio <---> Sobrinho (Relação de muitos para muitos)*/
+
+    }
+}
