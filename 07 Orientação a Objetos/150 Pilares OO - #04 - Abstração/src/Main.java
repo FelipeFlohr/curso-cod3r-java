69a07da551f959a797013cad77c4ab82ac823d3f
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-09 11:44:49 -0300

    Módulo 150

---
+++ 07 Orientação a Objetos/150 Pilares OO - #04 - Abstração/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Abstração
+
+        /* Abstração pode ser entendido como simplificação.
+        Além disso, é o processo de esconder certos detalhes
+        e mostrar somente a informação essencial para o usuário.
+        Abstração pode ser implementada com classes abstratas
+        (classes restritas que não podem ser usadas para criar
+        objetos, e é somente acessível com herança) ou interfaces
+        */
+
+    }
+}
