e541e69b0405552f97f7dad3dac4abf3fd61066f
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-09 10:29:57 -0300

    Módulo 147

---
+++ 07 Orientação a Objetos/147 Pilares OO - #01 - Encapsulamento/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Pilares - Encapsulamento
+
+        /* Encapsulamento consiste em fazer com que dados "sen-
+        síveis" sejam escondidos dos usuários. Para fazer isso,
+        é necessário que:
+        - Classes/variáveis sejam declaradas como privadas
+        - Prover getters e setters públicos e atualizar a va-
+        riável privada.
+
+        Modificadores de acesso:
+        - private: atributos privados só podem ser acessados
+        dentro da própria classe. Para acessar esses métodos,
+        (Palavra reservada: private)
+        usa-se getters e setters públicos.
+        - Default (package): acessível somente dentro do mes-
+        mo pacote.
+        - protected: acessível somente dela, do pacote e de
+        classes herdadas (Palavra reservada: protected).
+        - public: qualquer lugar do sistema pode acessar (Pa-
+        lavra reservada: public).
+        */
+    }
+}
