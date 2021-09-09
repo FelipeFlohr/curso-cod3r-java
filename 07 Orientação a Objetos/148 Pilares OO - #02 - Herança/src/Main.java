19f86a25d32bbf3189c19faa5d3dbd378bd63926
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-09 10:58:22 -0300

    Método 148

---
+++ 07 Orientação a Objetos/148 Pilares OO - #02 - Herança/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Pilares - Herança
+
+        /* Herança é semelhante a composição, porém com algu-
+        mas diferenças, veja:
+
+        Composição              | Herança
+        (Tem um(a))             | (É um(a))
+        - Carro tem um motor.   | - Civic é um carro.
+        - Carro têm portas.     | - Gato é um mamífero.
+        - Casa tem uma cozinha. | - Maçã é uma fruta.
+
+        Com a herança, a classe filho herda todos os atribu-
+        tos e funções da classe pai.
+        Exemplos de herança: todas as classes herdam da classe
+        Object.
+
+        Mais um exemplo de herança:
+        
+        Animal -> Mamífero -> Mamífero aquático
+                           -> Mamífero terrestre
+               -> Anfíbio  -> Sapo
+
+        A classe do topo da hierarquia é a super classe (a 
+        mais genérica) e a da parte de baixo da hierarquia é
+        a sub classe (a mais específica)
+
+        Para sobrepor métodos herdados de uma classe, usa-se
+        a sobreposição de métodos através do @Override
+        */
+
+    }
+
+}
