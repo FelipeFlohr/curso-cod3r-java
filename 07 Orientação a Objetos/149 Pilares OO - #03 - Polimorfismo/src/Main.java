ac1fd73953de778aeb4e9f55e129c6449bd68dae
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-09 11:33:36 -0300

    Módulo 149

---
+++ 07 Orientação a Objetos/149 Pilares OO - #03 - Polimorfismo/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Polimorfismo
+
+        /* Poli (múltiplas) morfismo (formas)
+
+        Há dois tipos de polimorfismo: estático e dinâmico
+
+        - Estático: utiliza-se da sobrecarga de métodos. A so-
+        brecargas de método é o termo usado para designar múl-
+        tiplos tipos de métodos com o mesmo nome, exemplo:
+        opa(int a);
+        opa(String a);
+        opa(double a); etc...
+        O nome "estático" é justamente referenciado pois a ma-
+        neira com que é empregado é estático, ou seja, não acon-
+        tece durante a execução do código. Outra característica
+        é que polimorfismo estático e sobrecarga são a mesma coi-
+        sa (sinônimos).
+
+        - Dinâmico: o principal requisito é a herança. Exemplo:
+        a classe pai é Carro, e as subclasses são Civic e Ferra-
+        ri. Com isso, é possível instanciar as classes das se-
+        guintes maneiras:
+        Civic c = new Civic();
+        Carro c = new Civic(); <- Isso é possível por causa da herança
+        */
+
+    }
+}
