1b9a3c2f96742bf50bfbe800c5fef90f2ae310fa
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-09 07:21:15 -0300

    Módulo 145

---
+++ 07 Orientação a Objetos/145 Paradigmas/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Paradigmas de programação
+
+        /* Paradigmas são formas criadas de como estruturar
+        as ideias e o código em geral. Alguns exemplos de para-
+        digmas:
+
+        - Não estruturado (GoTo): código na qual a sequência do
+        algoritmo é definido com Goto. Exemplo: o código possui
+        três blocos de código. Dessa forma, o primeiro é execu-
+        tado, porém o Goto manda pular para o último e somente
+        depois o código do meio é executado. Também é chamado de
+        estrutura "macarrônica".
+
+        - Procedural: código é executado na sequência do algorit-
+        mo. A partir desse ponto as estruturas de controle já es-
+        tavam melhores definidas dentro da programação. Também foi
+        introduzida as funções, variáveis, etc.
+
+        - Funcional: também é baseado em funções porém as estraté-
+        gias são diferentes. Funciona melhor com múltiplos núcleos
+        de processamento e utiliza-se bastante de constantes.
+
+        - Orientação a objetos: a classe define o tipo, e quando
+        você instancia os objetos, você cria os dados. O mesmo ob-
+        jeto possui atributos e comportamentos que modificam o da-
+        do.
+
+              Procedural     <- ator principal é a função
+                  vs
+        Orientação a objetos <- ator principal é o objeto
+
+        Procedural: formatarData(nascimento); um método altera o dado
+        Orientação a objetos: nascimento.formatarData(); o objeto (nascimento) possui o comportamento dentro dele
+
+        O Java em si é uma língua extremamente focada em orientação
+        a objetos, pois tudo gira em torno de classes. Já em outras
+        línguas, como Python e JavaScript, pode-se usar tanto para-
+        digmas procedurais, funcionais e OO para programar.
+        */
+
+    }
+}
