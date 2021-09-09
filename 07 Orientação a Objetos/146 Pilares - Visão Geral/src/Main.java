f83ff7b5788df387acc68d693edbdd8d89e68ebf
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-09 10:04:11 -0300

    Módulo 146

---
+++ 07 Orientação a Objetos/146 Pilares - Visão Geral/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        // Pilares - Visão geral
+
+        /* Há quatro pilares:
+        - Encapsulamento
+        - Herança
+        - Polimorfismo
+        - Abstração
+
+        - Encapsulamento: parte da complexidade do objeto pre-
+        cisa estar escondida para o mundo exterior. Exemplo:
+        em um carro, o usuário interage diretamente com as mar-
+        chas, pedais, volante, etc. Porém não necessariamente
+        o mesmo precisa saber de todos os componentes do motor.
+
+        - Herança: princípio de reuso de código. Na composição,
+        o usuário relaciona as classes através de relações "tem",
+        exemplo: o cliente têm uma compra, a compra têm um produto,
+        etc. E na herança, quando relacionamos, por exemplo, um car-
+        ro e um Celta, a relação fica: um Celta é um carro, um Cor-
+        sa é um carro, um Gol é um carro, etc.
+
+        - Polimorfismo: utilizar um conceito genérico e acrescentar
+        coisas específicas. Por exemplo: criar um atributo "Animal"
+        e associa-lo á um cachorro, a um gato, etc.
+
+        - Abstração: quando você vai montar um sistema, observa-se
+        o mundo real. Com isso, transcreve-se a complexidade do mun-
+        do real para dentro do programa através de abstração.
+        */
+    }
+}
