376a7692d67e12e727bb5ddea0340e8e55a3661e
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-16 16:46:16 -0300

    Módulo 178

---
+++ 08 Lambdas/178 Resumo Interfaces Funcionais/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Resumo Interfaces Funcionais
+
+        /* Tipos de Interfaces funcionais
+        sendo:
+        T = Tipo
+        S = Outro tipo
+        nada = sem parâmetro
+
+        Assinatura (Entrada -> Saída) | Nome
+        ----------------------------------------------------
+        nada -> T                     | Supplier
+        T -> nada                     | Consumer
+        T -> T                        | Unary Operation
+        T, T -> T                     | Binary Operation
+        S -> T                        | Function
+        T -> boolean                  | Predicate
+        ----------------------------------------------------
+        Interfaces p/ tipos primitivos:
+
+        -> IntBinaryOperator
+        -> LongBinaryOperator
+        */
+    }
+}
