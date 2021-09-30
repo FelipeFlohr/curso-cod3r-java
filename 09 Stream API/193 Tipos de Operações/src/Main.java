5ad67561bda513d49a091fc8c48afccfa20229cb
Felipe Flohr <felipeflohrlol@gmail.com>
2021-09-30 13:31:14 -0300

    Módulo 193

---
+++ 09 Stream API/193 Tipos de Operações/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Tipos de operações
+        /* Há três grandes grupos de operações
+        - Built Ops
+        - Int. Ops (Intermediary)
+        - Terminal Ops
+
+        O método Built serve justamente para construir uma Stream. Os inter-
+        mediary ops também retornam Streams, porém modificam-as. Já as termi-
+        nal ops servem para finalizar o algoritmo, e assim podem retornar va-
+        lores diferentes de Streams.
+
+        Sequências ordenadas geram valores que serão utilizados de forma orde-
+        nada, e as sequências não ordenadas irão gerar valores que serão utili-
+        zados de formas não ordenadas.
+
+        Streams executam tarefas em sequência, exemplo: digamos que há 10 ele-
+        mentos dentro de uma stream, e esses 10 precisam ser processados, então
+        a tarefa será executada de forma sequêncial (de uma em uma).
+        As streams paralelas (Paralel Stream) executam tarefas simultâneamente,
+        exemplo: digamos que há 10 elementos dentro de uma stream, e esses 10
+        precisam ser processados, então a tarefa será executada simultâneamente
+        para todos.
+         */
+    }
+}
