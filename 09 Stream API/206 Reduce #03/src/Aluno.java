7959950d764bea23193121788879ce52958cca5d
Felipe Flohr <felipeflohrlol@gmail.com>
2021-10-04 18:01:30 -0300

    Módulo 206

---
+++ 09 Stream API/206 Reduce #03/src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+
+    final String nome;
+    final double nota;
+    final boolean bomComportamento = true;
+
+    Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+}
