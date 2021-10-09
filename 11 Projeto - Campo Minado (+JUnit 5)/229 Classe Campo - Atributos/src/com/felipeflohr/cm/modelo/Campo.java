fd45880857b2effeffa36b9668348bc7b6951e9b
Felipe Flohr <felipeflohrlol@gmail.com>
2021-10-09 10:57:52 -0300

    Módulo 229

---
+++ 11 Projeto - Campo Minado (+JUnit 5)/229 Classe Campo - Atributos/src/com/felipeflohr/cm/modelo/Campo.java
@@ -0,0 +1,25 @@
+package com.felipeflohr.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false; // Quando o jogo identifica que há uma mina no campo
+	
+	private List<Campo> vizinhos = new ArrayList<>();
+
+	public Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho(Campo vizinho) {
+		
+	}
+}
