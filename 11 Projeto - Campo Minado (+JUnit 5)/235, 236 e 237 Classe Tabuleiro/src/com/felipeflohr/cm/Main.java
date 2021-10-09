7dfe33dda5d78cc8beb10e8b7f6d63bfca55910b
Felipe Flohr <felipeflohrlol@gmail.com>
2021-10-09 19:06:24 -0300

    Módulo 235, 236 e 237

---
+++ 11 Projeto - Campo Minado (+JUnit 5)/235, 236 e 237 Classe Tabuleiro/src/com/felipeflohr/cm/Main.java
@@ -0,0 +1,12 @@
+package com.felipeflohr.cm;
+
+import com.felipeflohr.cm.modelo.Tabuleiro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+	
+		System.out.println(tabuleiro);
+	}
+}
