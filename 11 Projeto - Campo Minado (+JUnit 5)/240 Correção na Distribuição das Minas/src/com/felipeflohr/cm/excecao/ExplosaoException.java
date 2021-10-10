de2c34ec5b1b8a2e0c66542b296b5121945c8f92
Felipe Flohr <felipeflohrlol@gmail.com>
2021-10-10 09:50:08 -0300

    Módulo 240

---
+++ 11 Projeto - Campo Minado (+JUnit 5)/240 Correção na Distribuição das Minas/src/com/felipeflohr/cm/excecao/ExplosaoException.java
@@ -0,0 +1,10 @@
+package com.felipeflohr.cm.excecao;
+
+public class ExplosaoException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ 11 Projeto - Campo Minado (+JUnit 5)/240 Correção na Distribuição das Minas/src/com/felipeflohr/cm/modelo/Tabuleiro.java
@@ -0,0 +1,94 @@
+package com.felipeflohr.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+		
+		gerarCampos();
+		associarOsVizinhos();
+		sortearMinas();
+	}
+
+	public void abrir(int linha, int coluna) {
+		campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+			.findFirst()
+			.ifPresent(c -> c.abrir());;
+	}
+
+	public void alterarMarcacao(int linha, int coluna) {
+		campos.parallelStream()
+			.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+			.findFirst()
+			.ifPresent(c -> c.alternarMarcacao());;
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+	}
+
+	private void associarOsVizinhos() {
+		// Este método irá tentar associar todo mundo á vizinho, e conforme as con-
+		// dições baterem, realizará a execução
+		for (Campo c1: campos) {
+			for (Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = campo -> campo.isMinado();
+
+		do {
+			int aleatorio = (int) (Math.random() * campos.size()); // Irá selecionar um campo aleatório
+			campos.get(aleatorio).minar();
+			minasArmadas = campos.stream().filter(minado).count();
+		} while (minasArmadas < minas);
+	}
+
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(campo -> campo.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		int i = 0;
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < colunas; coluna++) {
+				sb.append(" ");
+				sb.append(campos.get(i));
+				sb.append(" ");
+				i++;
+			}
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+}
