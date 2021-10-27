374fc9b508b684f623eb1caace997f2f51cd1a18
Felipe Flohr <felipeflohrlol@gmail.com>
2021-10-27 13:12:16 -0300

    Módulo 259

--- 13 Java Desktop com Swing/257, 258 e 259 Campo Minado com Observer/src/com/felipeflohr/cm/modelo/Tabuleiro.java
+++ 13 Java Desktop com Swing/257, 258 e 259 Campo Minado com Observer/src/com/felipeflohr/cm/modelo/Tabuleiro.java
@@ -2,15 +2,18 @@ package com.felipeflohr.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class Tabuleiro {
+public class Tabuleiro implements CampoObservador {
 
 	private int linhas;
 	private int colunas;
 	private int minas;
 	
 	private final List<Campo> campos = new ArrayList<>();
+	private final List<Consumer<ResultadoEvento>> observadores =
+			new ArrayList<>();
 
 	public Tabuleiro(int linhas, int colunas, int minas) {
 		this.linhas = linhas;
@@ -20,6 +23,15 @@ public class Tabuleiro {
 		gerarCampos();
 		associarOsVizinhos();
 		sortearMinas();
+	}
+
+	public void registrarObservador(Consumer<ResultadoEvento> observador) {
+		observadores.add(observador);
+	}
+
+	private void notificarObservadores(boolean resultado) {
+		observadores.stream()
+				.forEach(o -> o.accept(new ResultadoEvento(resultado)));
 	}
 
 	public void abrir(int linha, int coluna) {
@@ -45,7 +57,9 @@ public class Tabuleiro {
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
 			for (int coluna = 0; coluna < colunas; coluna++) {
-				campos.add(new Campo(linha, coluna));
+				Campo campo = new Campo(linha, coluna);
+				campo.registrarObservador(this);
+				campos.add(campo);
 			}
 		}
 	}
@@ -107,5 +121,21 @@ public class Tabuleiro {
 		}
 
 		return sb.toString();
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		if(evento == CampoEvento.EXPLODIR) {
+			mostrarMinas();
+			notificarObservadores(false);
+		} else if (objetivoAlcancado()) {
+			notificarObservadores(true);
+		}
+	}
+
+	private void mostrarMinas() {
+		campos.stream()
+				.filter(c -> c.isMinado())
+				.forEach(c -> c.setAberto(true));
 	}
 }
