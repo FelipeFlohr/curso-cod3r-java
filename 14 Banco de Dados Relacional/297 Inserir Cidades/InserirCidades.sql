c080f7b7380459f8a59c236c267e7efcefc08417
Felipe Flohr <felipeflohrlol@gmail.com>
2021-11-26 16:58:07 -0300

    Corrigido valores

--- 14 Banco de Dados Relacional/297 Inserir Cidades/InserirCidades.sql
+++ 14 Banco de Dados Relacional/297 Inserir Cidades/InserirCidades.sql
@@ -1,9 +1,9 @@
 -- Inserir cidades
 insert into cidades (nome, area, estado_id)
-values ("Campinas", 795, 25);
+values ("Campinas", 795, 26);
 
 insert into cidades (nome, area, estado_id)
-values ("Niterói", 133.9, 25);
+values ("Niterói", 133.9, 26);
 
 insert into cidades (nome, area, estado_id)
 values ("Caruaru", 920.6, (select id from estados where sigla = "PE"));
