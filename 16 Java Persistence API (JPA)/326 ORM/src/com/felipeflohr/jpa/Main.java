e69230e8b9d275131d634fa036bdc0d2ddc0482c
Felipe Flohr <felipeflohrlol@gmail.com>
2021-12-03 18:49:59 -0300

    Módulo 326

---
+++ 16 Java Persistence API (JPA)/326 ORM/src/com/felipeflohr/jpa/Main.java
@@ -0,0 +1,10 @@
+package com.felipeflohr.jpa;
+
+public class Main {
+	/* ORM (Object Relational Mapper - Mapeamento Objeto Relacional)
+	 * 
+	 * Dentro do ORM, o mundo da POO e E/R precisarão se comunicar. Para fazer isso, os
+	 * objetos precisarão se mapear com as colunas das tabelas de alguma forma: através de
+	 * Annotations, de JSONs, de XMLs, etc.
+	 */
+}
