d2457cd5597dae3483098a148869702857d829a5
Felipe Flohr <felipeflohrlol@gmail.com>
2021-12-23 12:37:26 -0300

    Módulo 352

---
+++ 16 Java Persistence API (JPA)/352 Named Native Query/src/main/java/com/felipeflohr/jpa/Main.java
@@ -0,0 +1,17 @@
+package com.felipeflohr.jpa;
+
+import com.felipeflohr.jpa.infra.DAO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Obtendo média dos filmes utilizando SQL nativo
+		
+		DAO<NotaFilme> dao = new DAO<>(NotaFilme.class);
+		NotaFilme nota = dao.consultarUm("obterMediaGeralDosFilmes");
+		
+		System.out.println(nota.getMedia());
+		
+		dao.fechar();
+	}
+}
