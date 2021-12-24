16d7afcdeade94dc4a3ad507e1ad4c00493dc094
Felipe Flohr <felipeflohrlol@gmail.com>
2021-12-24 13:33:02 -0300

    Módulo 359

---
+++ 17 Modularidade em Java/359 Módulo vs Encapsulamento/src/com/felipeflohr/modularidade/Main.java
@@ -0,0 +1,9 @@
+package com.felipeflohr.modularidade;
+
+public class Main {
+
+	/* A modularidade em Java serve para encapsular arquivos dentro de um .jar (Java
+	 * Archive), na qual irá definir quais pacotes/arquivos serão expostos e quais serão
+	 * escondidos.
+	 */
+}
