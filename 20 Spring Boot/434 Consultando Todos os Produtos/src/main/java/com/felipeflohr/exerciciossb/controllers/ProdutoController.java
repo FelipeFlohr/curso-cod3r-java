1cc88e69a2499093cdba5be0ac882b12b2edc041
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-04 14:38:55 -0300

    Módulo 434

---
+++ 20 Spring Boot/434 Consultando Todos os Produtos/src/main/java/com/felipeflohr/exerciciossb/controllers/ProdutoController.java
@@ -0,0 +1,32 @@
+package com.felipeflohr.exerciciossb.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipeflohr.exerciciossb.model.entities.Produto;
+import com.felipeflohr.exerciciossb.model.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@PostMapping
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) { // A Annotation "Valid" serve para explicitar que este produto tem que ser válido
+		produtoRepository.save(produto);
+		return produto;
+	}
+
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+}
